96ff68a17fe2188023bb1361af1e81dc04911628
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2024-01-26 18:39:25 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LP_Virtual_Override_New
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
